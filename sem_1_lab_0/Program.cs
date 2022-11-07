cd9ed818178d45b41523fa6d5390af07c8f61e6f
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2022-11-08 01:53:37 +0200

    Test cases were created

--- sem_1_lab_0/Program.cs
+++ sem_1_lab_0/Program.cs
@@ -3,12 +3,25 @@
 class Program
 {
     public static void Main(string[] args)
-    {
+    { 
         //Write a program that determines whether the number n is prime.
 
+        /*
+         * case 1 num = 1
+         * case 2 num = 6
+         * case 3 num = 5
+         * case 4 num = 8
+        */
         int num = 0;
         string positiveResult = "\nThe number is prime\n";
         string negativeResult = "\nThe number is not prime\n";
         Console.WriteLine(negativeResult);
+
+        /*
+         * case 1 positiveResult
+         * case 2 negativeResult
+         * case 3 positiveResult
+         * case 4 negativeResult
+        */
     }
 }
