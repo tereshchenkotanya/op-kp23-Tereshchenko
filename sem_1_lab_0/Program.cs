de03f7d7c2759a50bbeb413588ba2ffb07b0d00b
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2022-11-08 01:31:39 +0200

    Prototype of the system

---
+++ sem_1_lab_0/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Write a program that determines whether the number n is prime.
+
+        int num = 0;
+        string positiveResult = "\nThe number is prime\n";
+        string negativeResult = "\nThe number is not prime\n";
+        Console.WriteLine(negativeResult);
+    }
+}
