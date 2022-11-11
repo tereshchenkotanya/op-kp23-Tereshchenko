29402d8c76705a9f341120a4194c3490602e1e94
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2022-11-12 00:11:10 +0200

    Prototype of the system

---
+++ sem_1_lab_0/Task_4/Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Calculate the trigonometric function sin(x)
+
+        double x = 0;
+        int accuracy = 5;
+        double sin_x = 0;
+
+        Console.WriteLine("sin(x) = {0}", sin_x);
+
+    }
+}
