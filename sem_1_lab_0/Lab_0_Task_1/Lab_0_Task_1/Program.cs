4c1a0814065f6151eb7d497004a6472dbc3640fa
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2022-11-12 23:40:55 +0200

    Full implementation of the system

--- sem_1_lab_0/Lab_0_Task_1/Lab_0_Task_1/Program.cs
+++ sem_1_lab_0/Lab_0_Task_1/Lab_0_Task_1/Program.cs
@@ -10,21 +10,69 @@ class Program
          * case 1 x0 = 0, xn = 2, n = 1;
          * case 2 x0 = 3, xn = 5, n = 2;
          */
-        double x0 = 0;
-        double xn = 2;
-        int x_number = 0;
-        int n = 1;
+
+        double x0;
+        double xn;
+        int n;
+        char restart;
         double b = 2.5;
 
-        for (int i = 0; i <= n; i++)
+        Console.WriteLine("Function tabulation");
+        Console.WriteLine("-------------------\n");
+
+        while (true)
         {
-            double x;
-            double y;
-            x = x0 + i * (xn - x0) / n;
-            y = 9 * (x + 15 * Math.Sqrt(Math.Pow(x, 3) + Math.Pow(b, 3)));
-            Console.WriteLine("x" + x_number + " = " + x + ", " + "y(x) = " + y);
-            x_number++;
+            Console.WriteLine("Enter initial value of x0:");
+            x0 = Convert.ToDouble(Console.ReadLine());
+
+            if (x0 < -2.5)
+            {
+                Console.WriteLine("The entered value does not correspond to the function definition area.Try again!");
+                Console.WriteLine("\nEnter initial value of x0:");
+                x0 = Convert.ToDouble(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter final value of x:");
+            xn = Convert.ToDouble(Console.ReadLine());
+
+            if (xn <= x0)
+            {
+                Console.WriteLine("The number xn must be greater than the number x0. Try again!");
+                Console.WriteLine("\nEnter initial value of xn:");
+                x0 = Convert.ToDouble(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter number of intermediate values from x0 to xn:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= n; i++)
+            {
+                double x;
+                double y;
+                x = x0 + i * (xn - x0) / n;
+                y = 9 * (x + 15 * Math.Sqrt(Math.Pow(x, 3) + Math.Pow(b, 3)));
+                Console.WriteLine("x" + i + " = " + x + ", " + "y(x) = " + y);
+            }
+
+            Console.WriteLine("\nWould you like to try again? Write Y if yes and N if no:");
+            restart = Convert.ToChar(Console.ReadLine()!);
+            while (Char.ToUpper(restart) != 'Y' && Char.ToUpper(restart) != 'N')
+            {
+                Console.WriteLine("Please, put correct char! Write Y if yes and N if no:");
+                restart = Convert.ToChar(Console.ReadLine()!);
+            }
+
+            if (Char.ToUpper(restart) == 'Y')
+            {
+                Console.WriteLine('\n');
+                continue;
+            }
+            else if (Char.ToUpper(restart) == 'N')
+            {
+                break;
+            }
         }
+
         /*
          * case 1 x0 = 0, y(x) = 533,634355153414
                   x1 = 2, y(x) = 674,1749957137959
