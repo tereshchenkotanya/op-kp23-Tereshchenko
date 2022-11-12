0853eb8a521df99665e96d8185b70d974a8c0fc1
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2022-11-12 23:08:50 +0200

    Prototype of the system

---
+++ sem_1_lab_0/Lab_0_Task_1/Lab_0_Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Function tabulation
+        double x0 = 0;
+        double xn = 2;
+        int x_number = 0;
+        int n = 1;
+        double b = 2.5;
+
+        for (int i = 0; i <= n; i++)
+        {
+            double x;
+            double y;
+            x = x0 + i * (xn - x0) / n;
+            y = 9 * (x + 15 * Math.Sqrt(Math.Pow(x, 3) + Math.Pow(b, 3)));
+            Console.WriteLine("x" + x_number + " = " + x + ", " + "y(x) = " + y);
+            x_number++;
+        }
+    }
+}
