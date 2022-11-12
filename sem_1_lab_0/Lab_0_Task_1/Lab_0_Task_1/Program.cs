8ee0829038e0886a7c0f9073e8779555b31df7e7
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2022-11-12 23:35:19 +0200

    Test cases were created

--- sem_1_lab_0/Lab_0_Task_1/Lab_0_Task_1/Program.cs
+++ sem_1_lab_0/Lab_0_Task_1/Lab_0_Task_1/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     public static void Main(string[] args)
     {
         //Function tabulation
+        /*
+         * case 1 x0 = 0, xn = 2, n = 1;
+         * case 2 x0 = 3, xn = 5, n = 2;
+         */
         double x0 = 0;
         double xn = 2;
         int x_number = 0;
@@ -20,5 +25,12 @@ class Program
             Console.WriteLine("x" + x_number + " = " + x + ", " + "y(x) = " + y);
             x_number++;
         }
+        /*
+         * case 1 x0 = 0, y(x) = 533,634355153414
+                  x1 = 2, y(x) = 674,1749957137959
+           case 2 x0 = 3, y(x) = 908,3856278610402
+                  x1 = 4, y(x) = 1240,6433600862954
+                  x2 = 5, y(x) = 1645,9030654602418
+         */
     }
 }
