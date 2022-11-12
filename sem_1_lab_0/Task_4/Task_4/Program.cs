e6dd74d1bd2d799f1e284c4cb5f0411abf7b6d4d
tereshchenkotanya <114068751+tereshchenkotanya@users.noreply.github.com>
2022-11-13 01:05:13 +0200

    Changed test cases

--- sem_1_lab_0/Task_4/Task_4/Program.cs
+++ sem_1_lab_0/Task_4/Task_4/Program.cs
@@ -5,21 +5,21 @@ class Program
     {
         //Calculate the trigonometric function sin(x)
         /*
-         * case 1 x = 0, accuracy = 5;
-         * case 2 x = 0.5, accuracy = 5;
-         * case 3 x = 0, accuracy = 10;
-         * case 4 x = 0.5, accuracy = 10;
+         * case 1 x = 0, precision = 1;
+         * case 2 x = 0.5, precision = 0,0000001;
+         * case 3 x = 0, precision = 0,0000001;
+         * case 4 x = 0.5, precision = 1;
         */
         double x = 0;
-        int accuracy = 5;
+        int precision = 5;
         double sin_x = 0;
 
         Console.WriteLine("sin(x) = {0}", sin_x);
         /*
          * case 1 sin_x = 0;
-         * case 2 sin_x = 0,4794255386164159;
+         * case 2 sin_x = 0,479425538604203;
          * case 3 sin_x = 0;
-         * case 4 sin_x = 0,479425538604203;
+         * case 4 sin_x = 0,5;
         */
     }
 }
