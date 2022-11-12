fae7f7ed5ee359f172bf1a55abcda371d3a836cb
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2022-11-13 01:06:54 +0200

    Full implementation of the system

--- sem_1_lab_0/Task_4/Task_4/Program.cs
+++ sem_1_lab_0/Task_4/Task_4/Program.cs
@@ -1,6 +1,41 @@
 ﻿using System;
 class Program
 {
+    static double Fuctorial(int a)
+    {
+        double factorial = 1;
+
+        for (int i = 1; i <= a; i++)
+        {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    static double Degree(int degree, double x_org)
+    {
+        double x = x_org;
+
+        if (degree == 0)
+        {
+            return 1;
+        }
+        for (int i = 1; i < degree; i++)
+        {
+            x *= x_org;
+        }
+
+        return x;
+    }
+    static double Abs(double num)
+    {
+        if (num >= 0)
+        {
+            return num;
+        }
+
+        return -num;
+    }
     static void Main(string[] args)
     {
         //Calculate the trigonometric function sin(x)
@@ -10,11 +45,63 @@ class Program
          * case 3 x = 0, accuracy = 10;
          * case 4 x = 0.5, accuracy = 10;
         */
-        double x = 0;
-        int accuracy = 5;
-        double sin_x = 0;
 
-        Console.WriteLine("sin(x) = {0}", sin_x);
+        double x;
+        double t;
+        double precision;
+        char restart;
+
+        Console.WriteLine("Calculate the trigonometric function sin(x)");
+        Console.WriteLine("-------------------------------------------\n");
+        while (true)
+        {
+            double sin_x = 0;
+            int n = 0;
+            int x_degree;
+
+            Console.WriteLine("Enter value for x:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter value for precision:");
+            precision = Convert.ToDouble(Console.ReadLine());
+
+            while (true)
+            {
+                x_degree = 2 * n + 1;
+                double v1 = (double)Degree(n, -1);
+                double v2 = (double)Degree(x_degree, x);
+                double v3 = (double)Fuctorial(x_degree);
+
+                t = v1 * v2 / v3;
+                sin_x += t;
+                n++;
+
+                if (Abs(t) < precision)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("sin(x) = {0}", sin_x);
+            Console.WriteLine("\nWould you like to try again? Write Y if yes and N if no:");
+            restart = Convert.ToChar(Console.ReadLine()!);
+
+            while (Char.ToUpper(restart) != 'Y' && Char.ToUpper(restart) != 'N')
+            {
+                Console.WriteLine("Please, put correct char! Write Y if yes and N if no:");
+                restart = Convert.ToChar(Console.ReadLine()!);
+            }
+
+            if (Char.ToUpper(restart) == 'Y')
+            {
+                Console.WriteLine('\n');
+                continue;
+            }
+            else if (Char.ToUpper(restart) == 'N')
+            {
+                break;
+            }
+        }
         /*
          * case 1 sin_x = 0;
          * case 2 sin_x = 0,4794255386164159;
