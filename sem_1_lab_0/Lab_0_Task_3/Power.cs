5210ac1709539386a0181cbc26b237d71bf1324a
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2022-11-14 01:13:02 +0200

    Prototype of the power system

---
+++ sem_1_lab_0/Lab_0_Task_3/Power.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Calculation of the value of x^n
+
+        int n = 2;
+        int x_org = 2;
+        x_org = 4;
+
+        Console.WriteLine("x^n = " + x_org)
+    }
+}
