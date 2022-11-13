d6fad4d290f5d1dda9af5039a2390f4e8bcd8a1c
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2022-11-13 23:39:43 +0200

    Prototype of the system 1

---
+++ sem_1_lab_0/Lab_0_Task_1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Function tabulation
+
+        double x0 = 0;
+        double xn = 2;
+        int n = 1;
+        double b = 2.5;
+
+        double x = 0;
+        int i = 1;
+        double y = 559.445842022628;
+
+        Console.WriteLine("x{0} = {1}, y(x) = {2}", i, x, y);
+    }
+}
