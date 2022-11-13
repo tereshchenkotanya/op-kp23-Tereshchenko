4bce0cc0849b7b759f687245ee1d1c24e1b8b3cb
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2022-11-14 00:55:29 +0200

    Test cases for system 2.1 were created

--- sem_1_lab_0/Lab_0_Task_3/Factorial.cs
+++ sem_1_lab_0/Lab_0_Task_3/Factorial.cs
@@ -4,9 +4,19 @@ class Program
     public static void Main(string[] args)
     {
         //Calculation of the factorial
+        /*case 1 n = 1;
+        case 2 n = 2;
+        case 3 n = 3;
+        case 4 n = 4;
+        */
         int n = 1;
         int factorial = 1;
 
         Console.WriteLine("factorial = " + factorial);
+        /*case 1 factorial = 1;
+        case 2 factorial = 2;
+        case 3 factorial = 6;
+        case 4 factorial = 24;
+        */
     }
 }
