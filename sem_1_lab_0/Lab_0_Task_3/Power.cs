bcbcc111e254ca796007737c72deed6b50c76120
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2022-11-14 01:30:21 +0200

    Test cases for power system were created

--- sem_1_lab_0/Lab_0_Task_3/Power.cs
+++ sem_1_lab_0/Lab_0_Task_3/Power.cs
@@ -4,11 +4,23 @@ class Program
     public static void Main(string[] args)
     {
         // Calculation of the value of x^n
+        
+        /*case 1 x_org = 2; n = 2;
+        case 2 x_org = 2; n = -1; 
+        case 3 x_org = 10; n = 0; 
+        case 4 x_org = 1; n = 5; 
+        */
 
         int n = 2;
         int x_org = 2;
         x_org = 4;
 
         Console.WriteLine("x^n = " + x_org)
+        /*
+        case 1 x_org = 4;
+        case 2 x_org = 0.5;
+        case 3 x_org = 1;
+        case 4 x_org = 5;
+        */
     }
 }
