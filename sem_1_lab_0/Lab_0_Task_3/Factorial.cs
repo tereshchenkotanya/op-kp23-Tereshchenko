1e0acca1368818f8b57644296c5c680e86505b97
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2022-11-14 00:52:42 +0200

    Prototype of the system 3.1

---
+++ sem_1_lab_0/Lab_0_Task_3/Factorial.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Calculation of the factorial
+        int n = 1;
+        int factorial = 1;
+
+        Console.WriteLine("factorial = " + factorial);
+    }
+}
