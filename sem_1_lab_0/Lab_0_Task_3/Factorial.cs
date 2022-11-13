ddeb587f232996f29c5d7708152f421efa9446f7
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2022-11-14 00:59:02 +0200

    Full implementation of the factorial system

--- sem_1_lab_0/Lab_0_Task_3/Factorial.cs
+++ sem_1_lab_0/Lab_0_Task_3/Factorial.cs
@@ -9,8 +9,26 @@ class Program
         case 3 n = 3;
         case 4 n = 4;
         */
-        int n = 1;
+        //Calculation of the factorial
+        int num;
+
+        Console.WriteLine("Calculation of the factorial");
+        Console.WriteLine("----------------------------\n");
+
         int factorial = 1;
+        string negativeResult = "The number cannot be negative. Try again!";
+        Console.WriteLine("Enter the number:");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num < 0)
+        {
+            Console.WriteLine(negativeResult);
+        }
+
+        for (int i = 1; i <= num; i++)
+        {
+            factorial *= i;
+        }
 
         Console.WriteLine("factorial = " + factorial);
         /*case 1 factorial = 1;
