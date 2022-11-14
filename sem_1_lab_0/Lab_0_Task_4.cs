90f2658fc761fb0fdd626e1db1b21a36e8739e7d
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2022-11-14 02:13:44 +0200

    Test cases for system 4 were created

--- sem_1_lab_0/Lab_0_Task_4.cs
+++ sem_1_lab_0/Lab_0_Task_4.cs
@@ -4,11 +4,23 @@ class Program
     static void Main(string[] args)
     {
         //Calculate the trigonometric function sin(x)
+        /*
+         * case 1 x = 0, precision = 1;
+         * case 2 x = 0.5, precision = 0,0000001;
+         * case 3 x = 0, precision = 0,0000001;
+         * case 4 x = 0.5, precision = 1;
+        */
 
         double x = 0;
         double precision = 1;
         double sin_x = 0;
 
         Console.WriteLine("sin(x) = {0}", sin_x);
+        /*
+          * case 1 sin_x = 0;
+          * case 2 sin_x = 0,479425538604203;
+          * case 3 sin_x = 0;
+          * case 4 sin_x = 0,5;
+         */
     }
 }
