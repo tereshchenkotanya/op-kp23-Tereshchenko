caf2d7f72e2abb636ae9f2036ba1061b852a0c98
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2022-11-14 02:17:54 +0200

    Full implementation of the system 4

--- sem_1_lab_0/Lab_0_Task_4.cs
+++ sem_1_lab_0/Lab_0_Task_4.cs
@@ -1,6 +1,41 @@
 ﻿using System;
 class Program
 {
+    static double Factor(int a)
+    {
+        double factorial = 1;
+
+        for (int i = 1; i <= a; i++)
+        {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    static double Pow(int degree, double x_org)
+    {
+        double x = x_org;
+
+        if (degree == 0)
+        {
+            return 1;
+        }
+        for (int i = 1; i < degree; i++)
+        {
+            x *= x_org;
+        }
+
+        return x;
+    }
+    static double Abs(double num)
+    {
+        if (num >= 0)
+        {
+            return num;
+        }
+
+        return -num;
+    }
     static void Main(string[] args)
     {
         //Calculate the trigonometric function sin(x)
@@ -11,11 +46,41 @@ class Program
          * case 4 x = 0.5, precision = 1;
         */
 
-        double x = 0;
-        double precision = 1;
+        double x;
+        double t;
+        double precision;
+
+        Console.WriteLine("Calculate the trigonometric function sin(x)");
+        Console.WriteLine("-------------------------------------------\n");
         double sin_x = 0;
+        int n = 0;
+        int x_degree;
+
+        Console.WriteLine("Enter value for x:");
+        x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter value for precision:");
+        precision = Convert.ToDouble(Console.ReadLine());
+
+        while (true)
+        {
+            x_degree = 2 * n + 1;
+            double sign = (double)Pow(n, -1);
+            double power = (double)Pow(x_degree, x);
+            double factorial = (double)Factor(x_degree);
+
+            t = sign * power / factorial;
+            sin_x += t;
+            n++;
+
+            if (Abs(t) < precision)
+            {
+                break;
+            }
+        }
 
         Console.WriteLine("sin(x) = {0}", sin_x);
+        
         /*
           * case 1 sin_x = 0;
           * case 2 sin_x = 0,479425538604203;
