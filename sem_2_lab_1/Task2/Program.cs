5bf153bc4a2c9af5c03946462b52b4dc24974a85
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2023-03-03 15:51:56 +0200

    The prototype of the program 2 was created

---
+++ sem_2_lab_1/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //The max number of line
+
+        string allNumbersFilePath = "C:\\Users\\Таня\\OneDrive\\Documents\\KPI\\прога\\lab_2sem\\Lab1\\Task2\\bin\\Debug\\net6.0\\numbers.txt";
+        string maxNumberFilePath = "C:\\Users\\Таня\\OneDrive\\Documents\\KPI\\прога\\lab_2sem\\Lab1\\Task2\\bin\\Debug\\net6.0\\max.txt";
+        int maxNumber = 0;
+        Random rand = new Random();
+
+        using (StreamWriter sw = File.CreateText(allNumbersFilePath))
+        {
+            
+        }
+
+        using (StreamWriter sw = File.CreateText(maxNumberFilePath))
+        {
+        }
+
+    }
+}
