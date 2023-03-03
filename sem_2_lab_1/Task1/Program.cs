e57eee25bb4e43ee2962a385346def38b99cfa2f
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2023-03-03 17:17:18 +0200

    The prototype of the system was created

---
+++ sem_2_lab_1/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    // Write strings into file
+    public static void Main(string[] args)
+    {
+        using (var sw = new StreamWriter("test.txt"))
+        {
+ 
+        }
+        using (StreamReader sr = File.OpenText("test.txt"))
+        {
+
+        }
+    }
+}
