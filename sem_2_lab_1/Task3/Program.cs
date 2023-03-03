859c4b1e84fd5467b04a9ec84b2c3abe7efabd11
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2023-03-03 16:31:22 +0200

    The prototype of the program 3 was created

---
+++ sem_2_lab_1/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Sorting
+        string inputWords = "C:\\Users\\Таня\\OneDrive\\Documents\\KPI\\прога\\lab_2sem\\Lab1\\Task3\\bin\\Debug\\net6.0\\randomWords.txt";
+        string outputWords = "C:\\Users\\Таня\\OneDrive\\Documents\\KPI\\прога\\lab_2sem\\Lab1\\Task3\\bin\\Debug\\net6.0\\sortWords.txt";
+        string line;
+        
+        string[] words = File.ReadAllLines(inputWords);
+
+        Sort(words);
+
+        File.WriteAllLines(outputWords, words);
+    }
+    //InsertionSort
+    static void Sort(string[] words)
+    {
+      
+    }
+    //Method Compare
+    static int Compare(string str1, string str2)
+    {
+ 
+    }
+}
