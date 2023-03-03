1dead209b6c454dfaa9aacee1c964b9751484b80
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2023-03-03 17:12:29 +0200

    Full implementation of the system 5

--- sem_2_lab_1/Task5/Program.cs
+++ sem_2_lab_1/Task5/Program.cs
@@ -21,6 +21,20 @@ class Program
         string[,] list = new string[1000, 2];
         int rowIndex = 0;
 
+        for (int i = 0; i < lines.Length; i++)
+        {
+            while (list[rowIndex+1, 0] != null)
+            {
+                rowIndex++;
+            }
+            SplitAndCount(list, lines[i], rowIndex);
+        }
+
+        while (list[rowIndex + 1, 0] != null)
+        {
+            rowIndex++;
+        }
+
         for (int i = 0; i < rowIndex; i++)
         {
             Console.WriteLine(list[i, 0] + ": " + list[i, 1]);
@@ -42,6 +56,43 @@ class Program
     }
     static void SplitAndCount(string[,] list, string line, int rowIndex)
     {
-       
+        string substring = "";
+        int numberOfWords;
+        bool flag;
+
+        for (int i = 0; i < line.Length; i++)
+        {
+            flag = false;
+            if (line[i] != ',' && line[i] != '.' && line[i] != ':' && line[i] != ';' && line[i] != '/' && line[i] != '-' && line[i] != ' ')
+            {
+                substring += line[i];
+            }
+            else
+            {
+                if (substring == "")
+                {
+                    continue;
+                }
+                substring = substring.ToLower();
+                for (int j = 0; j < rowIndex; j++)
+                {
+                    if(substring == list[j, 0])
+                    {
+                        flag = true;
+                        numberOfWords = Int32.Parse(list[j, 1]) + 1;
+                        list[j, 1] = numberOfWords.ToString();
+                        break;
+                    }
+                }
+                if (!flag)
+                {
+                    list[rowIndex, 0] = substring;
+                    list[rowIndex, 1] = "1";
+                    rowIndex++;
+                }
+                substring = "";
+            }           
+        }
+        list[rowIndex, 0] = substring;
     }
 }
