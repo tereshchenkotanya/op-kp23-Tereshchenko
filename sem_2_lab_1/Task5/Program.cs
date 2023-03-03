f8daa062e9a8fc67b6733c17397f48f0184d5d73
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2023-03-03 17:10:14 +0200

    Test cases for program 5 were created

--- sem_2_lab_1/Task5/Program.cs
+++ sem_2_lab_1/Task5/Program.cs
@@ -6,6 +6,15 @@ class Program
 {
     public static void Main(string[] args)
     {
+        /*test cases:
+        *case1: aa bb
+                bb aa
+        *case2: The first line
+                The second line
+        *case3: cat dog parrot
+                dog cat parrot
+                parrot dog cat
+        */
         string text = "C:\\Users\\Таня\\OneDrive\\Documents\\KPI\\прога\\lab_2sem\\Lab1\\Task5\\bin\\Debug\\net6.0\\text.txt";
 
         string[] lines = File.ReadAllLines(text);
@@ -16,6 +25,20 @@ class Program
         {
             Console.WriteLine(list[i, 0] + ": " + list[i, 1]);
         }
+        /*test cases:
+        *case1: aa: 2
+                bb: 2
+        *case2: The first line
+                The second line
+                the: 2
+                first: 1
+                line: 2
+                second: 1
+                line: 2
+        *case3: cat: 3
+                dog: 3
+                parrot: 3
+        */
     }
     static void SplitAndCount(string[,] list, string line, int rowIndex)
     {
