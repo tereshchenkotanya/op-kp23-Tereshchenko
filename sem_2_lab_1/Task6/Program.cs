5a2b24e39ea0b405cfa8fe065d16538bb529460a
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2023-03-08 00:06:17 +0200

    The full implementation of the system 6

--- sem_2_lab_1/Task6/Program.cs
+++ sem_2_lab_1/Task6/Program.cs
@@ -11,7 +11,6 @@ class Program
         string binaryFilePath2 = "students2.bin";
 
         string[] lines = File.ReadAllLines(csvFilePath);
-        
         /*test cases:
          * case1: John,Smith,87
                   Emily,Jones,96
@@ -30,7 +29,12 @@ class Program
         {
             foreach (string line in lines)
             {
-                
+                string[] fields = Split(line);
+                string name = fields[0];
+                string surname = fields[1];
+                int score = int.Parse(fields[2]);
+
+                writer.Write(name + "," + surname + "," + score);
             }
         }
 
@@ -43,18 +47,31 @@ class Program
             {
                 while (reader.BaseStream.Position != reader.BaseStream.Length)
                 {
-                    
+                    string stringData = reader.ReadString();
+                    string[] fields = Split(stringData);
+
+                    string name = fields[0];
+                    string surname = fields[1];
+                    int score = int.Parse(fields[2]);
+
+                    if (score > 95)
+                    {
+                        count++;
+
+                        writer.Write(name + ',' + score);
+                    }
                 }
             }
         }
         if (count > 0)
         {
-
+            Console.WriteLine("Number of students with score greater than 95: " + count);
         }
         else
         {
-
+            Console.WriteLine("Anyone has a great score");
         }
+
         /*test cases:
          * case1:Number of students with score greater than 95:1 
          * case2:Anyone has a great score
@@ -63,8 +80,26 @@ class Program
     }
     static string[] Split(string line)
     {
+        string substring = "";
+        int number = 0;
+        string[] splitData = new string[3];
+
+        for (int i = 0; i < line.Length; i++)
+        {
+            if (line[i] != ',')
+            {
+                substring += line[i];
+            }
+            else
+            {
+                splitData[number] = substring;
+                number++;
+                substring = "";
+            }
+        }
+        splitData[number] = substring;
+
         return splitData;
     }
 }
-
 
