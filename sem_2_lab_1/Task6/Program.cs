4c41899b71c24fff9dc56b47c015045587a08279
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2023-03-08 00:03:06 +0200

    The prototype of the program 6 was changed

--- sem_2_lab_1/Task6/Program.cs
+++ sem_2_lab_1/Task6/Program.cs
@@ -11,19 +11,14 @@ class Program
         string binaryFilePath2 = "students2.bin";
 
         string[] lines = File.ReadAllLines(csvFilePath);
-
-        // Create the binary file based on the CSV file
         
+        // Create the binary file based on the CSV file
+
         using (BinaryWriter writer = new BinaryWriter(File.Open(binaryFilePath, FileMode.Create)))
         {
             foreach (string line in lines)
             {
-                string[] fields = Split(line);
-                string name = fields[0];
-                string surname = fields[1];
-                int score = int.Parse(fields[2]);
-
-                writer.Write(name + "," + surname + "," + score);
+                
             }
         }
 
@@ -36,47 +31,23 @@ class Program
             {
                 while (reader.BaseStream.Position != reader.BaseStream.Length)
                 {
-                    string stringData = reader.ReadString();
-                    string[] fields = Split(stringData);
-
-                    string name = fields[0];
-                    string surname = fields[1];
-                    int score = int.Parse(fields[2]);
-
-                    if (score > 95)
-                    {
-                        count++;
-
-                        writer.Write(name + ',' + score);
-                    }
+                    
                 }
             }
         }
+        if (count > 0)
+        {
+            
+        }
+        else
+        {
 
-        Console.WriteLine("Number of students with score greater than 95: " + count);
+        }
     }
     static string[] Split(string line)
     {
-        string substring = "";
-        int number = 0;
-        string[] splitData = new string[3];
-
-        for (int i = 0; i < line.Length; i++)
-        {
-            if (line[i] != ',')
-            {
-                substring += line[i];
-            }
-            else
-            {
-                splitData[number] = substring;
-                number++;
-                substring = "";
-            }
-        }
-        splitData[number] = substring;
-
         return splitData;
     }
 }
+
 
