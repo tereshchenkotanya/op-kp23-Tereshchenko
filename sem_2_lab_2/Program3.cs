801ff5f4c89a0bd713c763ac48463ffe7544dab8
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2023-04-17 03:48:32 +0300

    The prototype of the program 3

---
+++ sem_2_lab_2/Program3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+abstract class Vessel
+{
+    public abstract void PrepareToMovement();
+    public abstract void Move();
+}
+class SailingVessel : Vessel
+{
+    public override void PrepareToMovement()
+    {
+
+    }
+    public override void Move()
+    {
+
+    }
+}
+class Submarine : Vessel
+{
+    public override void PrepareToMovement()
+    {
+
+    }
+    public override void Move()
+    {
+
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vessel sailingVessel = new SailingVessel();
+        Vessel submarine = new Submarine();
+
+        sailingVessel.PrepareToMovement();
+        sailingVessel.Move();
+
+        submarine.PrepareToMovement();
+        submarine.Move();
+    }
+}
