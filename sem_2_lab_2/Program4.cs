cb74f6751b21c20cb01ffe4fac6089db88b39833
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2023-04-17 03:51:18 +0300

    The prototype of the program 4

---
+++ sem_2_lab_2/Program4.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+interface IVessel
+{
+    void PrepareToMovement();
+    void Move();
+}
+
+class SailingVessel : IVessel
+{
+    public void PrepareToMovement()
+    {
+
+    }
+
+    public void Move()
+    {
+
+    }
+}
+
+class Submarine : IVessel
+{
+    public void PrepareToMovement()
+    {
+
+    }
+
+    public void Move()
+    {
+
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IVessel vessel1 = new SailingVessel();
+        IVessel vessel2 = new Submarine();
+
+        vessel1.PrepareToMovement();
+        vessel1.Move();
+
+        vessel2.PrepareToMovement();
+        vessel2.Move();
+    }
+}
