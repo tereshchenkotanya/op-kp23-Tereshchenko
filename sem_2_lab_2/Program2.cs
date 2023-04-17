0979249c317f4985012fb96a68915b965b3c9300
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2023-04-17 03:38:04 +0300

    The prototype of the program1 was created

---
+++ sem_2_lab_2/Program2.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+// Клас, що представляє відомість
+class Data
+{
+    // Поля відомості
+    private string surname;
+    private double salary;
+    private double taxes;
+    private double issuedEarnings;
+    public const double taxRate = 0.195;
+    // Конструктор для встановлення початкових значень полів
+    public Data(string name, double salary)
+    {
+        this.surname = name;
+        this.salary = salary;
+        // Обчислення значень розрахункових полів
+    }
+
+    // Метод для обчислення значення поля "податки"
+    public void CalculateTaxes()
+    {
+        // Податки - 19,5% від зарплати
+
+    }
+
+    // Метод для виведення відомості на консоль
+    public void DisplayTaxes()
+    {
+
+    }
+    public double GetSalary()
+    {
+        return salary;
+    }
+
+    // Метод для отримання суми податків
+    public double GetTaxes()
+    {
+        return taxes;
+    }
+
+    // Метод для отримання суми виплачених заробітків
+    public double GetIssuedEarnings()
+    {
+        return issuedEarnings;
+    }
+}
+
+//// Головний клас програми
+class Program
+{
+    static double CalculateSalarySum(Data[] data)
+    {
+        double sum = 0;
+
+        return sum;
+    }
+
+    // Метод для обчислення суми податків
+    static double CalculateTaxesSum(Data[] data)
+    {
+        double sum = 0;
+
+        return sum;
+    }
+
+    // Метод для обчислення суми виплачених заробітків
+    static double CalculateIssuedEarningsSum(Data[] data)
+    {
+        double sum = 0;
+
+        return sum;
+    }
+    static void Main()
+    {
+        bool validInput = false;
+        int n = 0;
+        string input;
+        string surname;
+        double salary;
+
+        // Створення масиву об'єктів класу "Відомість"
+        Data[] data = new Data[n];
+
+        // Уведення вихідних даних з консолі і створення об'єктів
+
+        int num;
+
+        // Виведення відомостей на консоль
+
+        double salarySum = CalculateSalarySum(data);
+        double taxesSum = CalculateTaxesSum(data);
+        double issuedEarnіngsSum = CalculateIssuedEarningsSum(data);
+    }
+}
+
