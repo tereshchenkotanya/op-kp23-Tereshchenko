f92a7c4cef71a1c8f1632484f8728eb921498a0b
tereshchenkotanya <tanyatereshchenko4@gmail.com>
2023-04-17 04:04:04 +0300

    Full implementation of the system5

--- sem_2_lab_2/Program5.cs
+++ sem_2_lab_2/Program5.cs
@@ -15,18 +15,57 @@ class Vector
 
         int sum = 0;
 
+        for (int i = 0; i < length1; i++)
+        {
+            if (v1.elements[i] >= 0)
+            {
+                continue;
+            }
+            else
+            {
+                sum += v1.elements[i];
+            }
+        }
+        for (int i = 0; i < length2; i++)
+        {
+            if (v2.elements[i] >= 0)
+            {
+                continue;
+            }
+            else
+            {
+                sum += v2.elements[i];
+            }
+        }
+        if (sum == 0) // Перевірка, чи сума від'ємних елементів дорівнює 0
+        {
+            Console.WriteLine("У векторах немає від'ємних елементів.");
+        }
         return sum;
     }
     public static int operator *(Vector v1, Vector v2)
     {
         int product = 1;
-
+        for (int i = 1; i < v1.elements.Length; i += 2)
+        {
+            product *= v1.elements[i];
+        }
+        for (int i = 1; i < v2.elements.Length; i += 2)
+        {
+            product *= v2.elements[i];
+        }
         return product;
     }
     public static int CountZeroElements(Vector v)
     {
         int count = 0;
-
+        for (int i = 0; i < v.elements.Length; i++)
+        {
+            if (v.elements[i] == 0)
+            {
+                count++;
+            }
+        }
         return count;
     }
 
@@ -58,31 +97,47 @@ class Program
         //        v2 = {0, -3}
         //        num = 3
 
-
+        Console.WriteLine("Enter number of vectors` elements");
         int n1 = Convert.ToInt32(Console.ReadLine());
         int n2 = Convert.ToInt32(Console.ReadLine());
 
         int[] v1 = new int[n1];
         int[] v2 = new int[n2];
 
+        Console.WriteLine("Write elements for v1");
+        for (int i = 0; i < n1; i++)
+        {
+            v1[i] = Convert.ToInt32(Console.ReadLine());
+        }
         Vector v1_1 = new Vector(v1);
 
+        Console.WriteLine("Write elements for v1");
+        for (int i = 0; i < n2; i++)
+        {
+            v2[i] = Convert.ToInt32(Console.ReadLine());
+        }
         Vector v2_1 = new Vector(v2);
 
-        int num;
+        Console.WriteLine("What do you want to know?");
+        Console.WriteLine("1) Sum\n2) Product\n3) Count of 0-elements");
+        int num = Convert.ToInt32(Console.ReadLine());
 
         if(num == 1)
         {
-
+            int sum = v1_1 + v2_1;
+            Console.WriteLine("The sum of the vectors elements: " + sum);
         }
         else if(num == 2)
         {
-
+            int product = v1_1 * v2_1;
+            Console.WriteLine("The product of the vectors elements: " + product);
         }
         else if(num == 3)
         {
-
+            int zeroElementsCount = v1_1 & v2_1;
+            Console.WriteLine("Number of zero elements of vectors: " + zeroElementsCount);
         }
+
         //test cases:
         //case 1: -5
         //case 2: -6
